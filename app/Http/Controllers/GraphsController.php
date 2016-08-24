c08e5710e551defca535ae72d2a4730a7f73e420
Chris Williams <chris_J_W@hotmail.com>
2016-08-24 20:51:56 +0930

    First Checkin

---
+++ app/Http/Controllers/GraphsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Carbon\Carbon;
+
+class GraphsController extends Controller
+{
+    public function day(Carbon $date) {
+
+    }
+
+    public function month(Carbon $date) {
+
+    }
+
+    public function year(Carbon $date) {
+        
+    }
+}
