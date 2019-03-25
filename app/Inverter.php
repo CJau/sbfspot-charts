30e0653b26f5b0aa709df4ff37ef714c3298e54e
Chris Williams <chris_J_W@hotmail.com>
2019-03-25 21:00:41 +1030

    Inverter table overrides

--- app/Inverter.php
+++ app/Inverter.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inverter extends Model
 {
-    protected $primaryKey = 'serial';
+    protected $primaryKey = 'Serial';
+    protected $table = 'Inverters';
     public $incrementing = false;
 
     public function data_points() {
