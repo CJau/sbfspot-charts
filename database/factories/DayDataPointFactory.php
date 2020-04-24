777807f77d2b56ff16b9199d97fe2fab68834f9d
Chris Williams <chris@cjs.id.au>
2020-04-24 20:49:45 +0930

    Basic test

--- database/factories/DayDataPointFactory.php
+++ database/factories/DayDataPointFactory.php
@@ -3,12 +3,13 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\DayDataPoint;
+use App\Inverter;
 use Faker\Generator as Faker;
 
 $factory->define(DayDataPoint::class, function (Faker $faker) {
     return [
         'TimeStamp' => $faker->time('U'),
-        'Serial' => $faker->unique()->word,
+        'Serial' => factory(Inverter::class)->create()->Serial,
         'TotalYield' => $faker->numberBetween(0, 90000000),
         'Power' => $faker->numberBetween(0, 2000),
         'PVOutput' => 0,
--- tests/Feature/GraphControllerTest.php
+++ tests/Feature/GraphControllerTest.php
@@ -3,22 +3,48 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class GraphControllerTest extends TestCase
 {
     use RefreshDatabase;
     
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    /** @test */
+    public function home_redeirects_to_day() {
+        $this->get('/')
+            ->assertRedirect(route('graphs.day'));
+    }
 
-        $response->assertStatus(200);
+    /** @test */
+    public function day_returns_expected_response()
+    {
+        $this->get(route('graphs.day'))
+            ->assertOk()
+            ->assertViewIs('graphs.day')
+            ->assertViewHas([
+                'date' => today()->endOfDay(),
+            ]);
+    }
+
+    /** @test */
+    public function month_returns_expected_response()
+    {
+        $this->get(route('graphs.month'))
+            ->assertOk()
+            ->assertViewIs('graphs.month')
+            ->assertViewHas([
+                'date' => today()->endOfDay(),
+            ]);
+    }
+
+    /** @test */
+    public function year_returns_expected_response()
+    {
+        $this->get(route('graphs.year'))
+            ->assertOk()
+            ->assertViewIs('graphs.year')
+            ->assertViewHas([
+                'date' => today()->endOfDay(),
+            ]);
     }
 }
