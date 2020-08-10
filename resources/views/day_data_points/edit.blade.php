60ae287b97be78d3f0e356806f0eac3bf3fc60f4
Chris Williams <25449733+CJau@users.noreply.github.com>
2020-08-10 20:50:53 +0930

    Edit form

--- resources/views/day_data_points/edit.blade.php
+++ resources/views/day_data_points/edit.blade.php
@@ -1,20 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container mx-auto px-6 md:px-0 space-y-4">
+  <div class="container px-6 mx-auto space-y-4 md:px-0">
     <div class="my-4">
-      <h2 class="md:inline-block align-middle">Updating point - {{ $dayDataPoint->TimeStamp->format('Y-m-d H:i:s') }}</h2>
+      <h2 class="align-middle md:inline-block">Updating point - {{ $dayDataPoint->TimeStamp->format('Y-m-d H:i:s') }}</h2>
     </div>
     
-    <label for="TimeStamp">Timestamp</label>
-    <input type="text" name="TimeStamp" value="{{ $dayDataPoint->TimeStamp->toDateTimeString() }}" class="form-input">
+    <form action="{{ route('day_data_points.update', [ $dayDataPoint->Serial, $dayDataPoint->TimeStamp->format('U') ])">
+        @csrf
+        @method('PATCH')
+        <label for="TimeStamp">Timestamp</label>
+        <input type="text" name="TimeStamp" value="{{ $dayDataPoint->TimeStamp->toDateTimeString() }}" class="form-input">
 
-    <label for="TotalYield">Total Yield</label>
-    <input type="text" name="TotalYield" value="{{ $dayDataPoint->TotalYield }}" class="form-input">
+        <label for="TotalYield">Total Yield</label>
+        <input type="text" name="TotalYield" value="{{ $dayDataPoint->TotalYield }}" class="form-input">
 
-    <label for="Power">Power</label>
-    <input type="text" name="Power" value="{{ $dayDataPoint->Power }}" class="form-input">
+        <label for="Power">Power</label>
+        <input type="text" name="Power" value="{{ $dayDataPoint->Power }}" class="form-input">
 
-    <input type="submit" value="Update" class="btn">
+        <input type="submit" value="Update" class="btn">
+    </form>
   </div>
 @endsection
